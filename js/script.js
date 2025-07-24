827aa5c440431e21705c8ff2f30d872f199c4d5d
Carlos Alberto da Silva <slayer.br@gmail.com>
2025-07-24 19:06:52 -0300

    Atualizando script.js com a função para atualizar valores do carrinho

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ btnAddToCart.forEach((button) => {
     const cart = getProductsFromCart();
 
     /* Verifica se o produto já existe no carrinho do localStorage */
-    const existProductInCart = cart.find((product) => product.id === productId);
+    const existProductInCart = cart.find((item) => item.id === productId);
 
     if (existProductInCart) {
       existProductInCart.quantity = (existProductInCart.quantity || 1) + 1;
@@ -36,8 +36,7 @@ btnAddToCart.forEach((button) => {
     }
 
     saveCartToLocalStorage(cart);
-    updateCartCount();
-    renderTableFromCart();
+    updateCartAndTable();
   });
 });
 
@@ -66,7 +65,6 @@ function updateCartCount() {
     cartCountElement.textContent = totalQuantity;
   }
 }
-updateCartCount();
 
 /* Função para renderiza os produtos para a Tabela do modal */
 function renderTableFromCart() {
@@ -87,7 +85,9 @@ function renderTableFromCart() {
       </td>
       <td class="td-description">${product.name}</td>
       <td class="td-price">R$ ${product.price.toFixed(2).replace(".", ",")}</td>
-      <td class="td-quantity"><input type="number" value="${product.quantity}" min="1" /></td>
+      <td class="td-quantity"><input type="number" class="input-quantify" data-id="${product.id}" value="${
+      product.quantity
+    }" min="1" /></td>
       <td class="td-total">R$ ${(product.price * product.quantity).toFixed(2).replace(".", ",")}</td>
       <td class="td-actions">
         <button class="remove-item" id="delete" data-id="${product.id}"></button>
@@ -98,9 +98,9 @@ function renderTableFromCart() {
   });
 }
 
-renderTableFromCart();
-
 const tableBody = document.querySelector("#modal-1-content table tbody");
+
+
 tableBody.addEventListener("click", (event) => {
   if (event.target.classList.contains("remove-item")) {
     const id = event.target.dataset.id;
@@ -109,10 +109,51 @@ tableBody.addEventListener("click", (event) => {
 });
 
 function removeProductToCart(id) {
-  const products = getProductsFromCart()
+  const products = getProductsFromCart();
   /* Filtra os  produtos sem o id do parâmetro*/
-  const cartUpdated = products.filter(product => product.id !== id)
-  saveCartToLocalStorage(cartUpdated)
-  updateCartCount()
-  renderTableFromCart()
+  const cartUpdated = products.filter((product) => product.id !== id);
+  saveCartToLocalStorage(cartUpdated);
+  updateCartAndTable();
 }
+
+tableBody.addEventListener("input", function (event) {
+  if (event.target.classList.contains("input-quantify")) {
+    const id = event.target.getAttribute("data-id");
+    let newQuantity = parseInt(event.target.value, 10);
+
+    if (isNaN(newQuantity) || newQuantity < 1) newQuantity = 1;
+
+    const cart = getProductsFromCart();
+    const product = cart.find((product) => product.id === id);
+
+    if (product) {
+      product.quantity = newQuantity;
+      saveCartToLocalStorage(cart);
+      updateCartAndTable();
+    }
+  }
+});
+
+function updateTotalCart() {
+  const cart = getProductsFromCart();
+
+  let total = 0;
+
+  cart.forEach((product) => {
+    total += product.price * product.quantity || 1;
+  });
+
+  const totalSpan = document.getElementById("total-price");
+
+  if (totalSpan) {
+    totalSpan.textContent = `Total: R$ ${total.toFixed(2).replace(".", ",")}`;
+  }
+}
+
+function updateCartAndTable() {
+  updateCartCount();
+  renderTableFromCart();
+  updateTotalCart();
+}
+
+updateCartAndTable();
